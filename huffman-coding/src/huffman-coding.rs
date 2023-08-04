f668101ffac042e3d7019dac65540e3a07c7580f
Dalmo Veras <d.verasnt@gmail.com>
2023-08-04 13:55:23 -0700

    add Huffman tree function

--- huffman-coding/src/huffman-coding.rs
+++ huffman-coding/src/huffman-coding.rs
@@ -46,6 +46,46 @@ impl<T: Clone> Tree<T>{
             Leaf { .. } => None,
         }
     }
+
+    impl<T: Clone + Eq> Ord for Tree<T>{
+        fn cmp(&self, alternative: &self)-> std::cmp::Ordering {
+            self.frequency().cmp(&alternative.frequency())
+        }
+    }
+
+    impl<T: Clone + Eq> PartialOrd for Tree<T>{
+        fn partial_cmp(&self, alternative: &self) -> Option<std::cmp::Ordering> {
+            Somee(self.cmp(alternative))
+        }
+    }
+
+    pub fn huffman<T: Eq + Clone>(frequencies: &HashMap<T, u64>)-> Tree<T>{
+        let mut heap = BinaryHeap::new();
+        for(token, frequency) in frequencies {
+            let (frequency, token) = (*frequency, token.clone());
+            //reverse so the most used token is close to the root
+            heap.push(Reverse(Leaf {frequency, token})); 
+        }
+
+        while heap.len() > 1 {
+            let node1 = heap.pop().unwrap().0;
+            let node2 = heap.pop().unwrap().0;
+
+            let combined = Node {
+                frequency: node1.frequency()+node2.frequency(),
+                letf_node: Box::new(node1),
+                right_node: Box::new(node2),
+            };
+            heap.push(Reversed(combined));
+        }
+        //Returning root
+        heap.pop().unwrap().0
+    }
+
+    #[cfg(test)]
+    fn huffman_test(){
+        
+    }
 }//end impl Tree
 
 
