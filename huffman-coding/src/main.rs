a5f34f4c6e06e6d2739c6b4a4476157800bd5b31
Dalmo Veras <d.verasnt@gmail.com>
2023-08-09 06:47:10 -0700

    add main function

--- huffman-coding/src/main.rs
+++ huffman-coding/src/main.rs
@@ -1,16 +1,97 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum Tree <T>{
-    Leaf {
-        freq: u64,
-        token: T,
-    },
-    Node {
-        freq: u64,
-        left: Box<Tree<T>>,
-        right: Box<Tree<T>>,
-    },
+use clap::{Parser, ValueEnum};
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::PathBuf;
+use std::time;
+
+mod compress;
+mod frequencies;
+mod huffman_coding;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(value_enum)]
+    action: Action,
+    #[arg(value_enum)]
+    mode: Mode,
+    input: PathBuf,
+    output: PathBuf,
 }
 
-pub fn main(){
-    todo!()
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum Action {
+    Compress,
+    Extract,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+enum Mode {
+    Words,
+    Chars,
+}
+
+// time zip data/test.zip data/wikisent2.txt
+// time unzip data/test.zip -d data/test_zip
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    match args.action {
+        Action::Compress => {
+            let timer = time::Instant::now();
+            let text = fs::read_to_string(args.input)?;
+            let lines: Vec<_> = text.split('\n').map(|x| x.to_string()).collect();
+            let time = timer.elapsed();
+            let lines_count = lines.len();
+            println!("Read the source file with {lines_count} lines in {time:?}");
+
+            let timer = time::Instant::now();
+            let compressed = match args.mode {
+                Mode::Words => compression::compress(&lines, freqs::word_frequencies, |line| {
+                    line.split_ascii_whitespace().map(|token| token.to_string())
+                }),
+                Mode::Chars => {
+                    compression::compress(&lines, freqs::char_frequencies, |line| line.chars())
+                }
+            }?;
+            let time = timer.elapsed();
+            println!("Compressed as {mode:?} in {time:?}.", mode = args.mode);
+
+            let timer = time::Instant::now();
+            let mut out_f = File::create(&args.output)?;
+            out_f.write(&compressed)?;
+            let time = timer.elapsed();
+            println!(
+                "Wrote to {output_path:?} in {time:?}",
+                output_path = args.output
+            );
+        }
+        Action::Extract => {
+            let timer = time::Instant::now();
+            let data = fs::read(&args.input)?;
+            let time = timer.elapsed();
+            println!("Read the compressed file in {time:?}");
+
+            let timer = time::Instant::now();
+            let content = match args.mode {
+                Mode::Words => compression::extract(&data, |tokens: Vec<String>| tokens.join(" "))?,
+                Mode::Chars => {
+                    compression::extract(&data, |tokens: Vec<char>| tokens.into_iter().collect())?
+                }
+            };
+            let time = timer.elapsed();
+            let lines_count = content.len();
+            println!("Extracted file with {lines_count} lines in {time:?}.");
+
+            let timer = time::Instant::now();
+            fs::write(&args.output, content.join("\n"))?;
+            let time = timer.elapsed();
+            println!(
+                "Wrote to {output_path:?} in {time:?}",
+                output_path = args.output
+            );
+        }
+    }
+
+    Ok(())
 }
